ad0d2b4ec12d8b40b0fc470265ca88218357c202
Joseph Kinahan <kinahanj25@gmail.com>
2020-10-16 16:11:35 +0100

    srrted apollo server/ index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require ("apollo-server");
+
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+    type Query{
+        sayHello: String!
+    }
+`
+const resolvers = {
+    Query: {
+        sayHello: () => 'Hello World!!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({ port: 5000})
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    });
+    
